3c65d12225bfe19f9157550a056a18cbde924078
Michael Pavars <michael.pavars@gmail.com>
2026-07-28 10:53:14 +0200

    Third Update - Correction of Text "I am a developer!"

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageOne = "I am a new developder!"
+    @State private var messageOne = "I am a developer!"
     var body: some View {
         VStack {
            

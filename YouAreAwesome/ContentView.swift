030238e109507e4855ce969748ba587ecd87e54f
Michael Pavars <michael.pavars@gmail.com>
2026-07-28 10:37:56 +0200

    First Update containing: - New Image - New Text - New Button

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageOne = "I am a developer!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(messageOne)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            Button("Drück mich!") {
+            messageOne = "Freibier!"
+            }
+            
         }
         .padding()
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+    

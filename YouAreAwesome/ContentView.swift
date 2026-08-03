ad738fc8bc47c0d64080161a99b2330587eae3b6
Michael Pavars <michael.pavars@gmail.com>
2026-08-03 09:36:21 +0200

    Added 2nd HStack with two Buttons.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,22 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageOne = "I am a developer!"
+    @State private var messageOne = "I am a Programmer!"
     var body: some View {
         VStack {
            
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(messageOne)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Drück mich!") {
-            messageOne = "Freibier!"
-            }
+                .fontWeight(.ultraLight)
+           
+            Spacer()
             
+            HStack {
+                Button("Awesome!") {
+                    messageOne = "Awesome!"
+                }
+                Button("Great!") {
+                    messageOne = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
         
